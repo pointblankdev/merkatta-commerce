114e9efcf0aeb2c91b6f5823b561c3062ecb043b
danstepanov <danhstepanov@gmail.com>
2020-12-20 23:09:16 -0800

    remove unused code

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -4,10 +4,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import type { Page } from '@bigcommerce/storefront-data-hooks/api/operations/get-all-pages'
 import getSlug from '@lib/get-slug'
-import { Github } from '@components/icons'
 import { Logo, Container } from '@components/ui'
-import { I18nWidget } from '@components/common'
-import s from './Footer.module.css'
 
 interface Props {
   className?: string
