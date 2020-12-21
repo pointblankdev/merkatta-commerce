019bdf982387f9f38a8ab20de869392d00802a52
danstepanov <danhstepanov@gmail.com>
2020-12-20 23:55:23 -0800

    add tab system for dashboard

---
+++ components/ui/Product/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@components/ui'
+
+export default function Product() {
+    return <Container>
+        <div className="mb-12 transition ease-in duration-75" >
+          <>There are no products</>
+        </div>
+    </Container>
+}
--- pages/vendor/dashboard.tsx
+++ pages/vendor/dashboard.tsx
@@ -1,52 +1,27 @@
 import cn from 'classnames'
 import { useState } from "react"
 import { Layout } from '@components/common'
-import { Button, Container } from '@components/ui'
-import Link from 'next/link'
+import { Container, Product, Settings } from '@components/ui'
 export default function Dashboard() {
-  const categories = {
-    0: {
-      "name": "product"
-    },
-    1: {
-      "name": "settings"
-    }
-  }
+  const categories = [{index: 0, label: 'Product'}, {index: 1, label: 'Settings'}]
   const [activeCategory, setActiveCategory] = useState(0)
 
   return <Container>
     <div className="grid grid-cols-12 gap-4 mt-3 mb-20">
       <div className="col-span-2">
-        <nav className="mb-10">
           <ul>
-          <li className="py-1 text-base font-bold tracking-wide">
-            <a>Vendor Dashboard</a>
-          </li>
-          <li className={cn("py-1 text-accents-8", {
-                  underline: activeCategory === 0 })}>
-            <a onClick={() => setActiveCategory(0)}>
-              Product
-            </a>
-          </li>
-          <li className={cn("py-1 text-accents-8", {
-            underline: activeCategory === 1
-          })}>
-            <a onClick={() => setActiveCategory(1)}>
-              Settings
-            </a>
-          </li>
+            <li className="py-1 text-base font-bold tracking-wide">
+              <a>Vendor Dashboard</a>
+            </li>
+            {categories.map(({ index, label }) => (
+              <li onClick={() => setActiveCategory(index)} className={cn("flex items-center mt-4 py-2 px-6 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100", { underline: activeCategory === index })}>
+                <a className="btn-blue">{label}</a>
+              </li>
+            ))}
           </ul>
-        </nav>
       </div>
       <div className="col-span-10">
-        {activeCategory === 0 && <div className="mb-12 transition ease-in duration-75" >
-          <>There are no products</>
-        </div>
-        }
-        {activeCategory === 1 && <div className="mb-12 transition ease-in duration-75" >
-          <>There are no settings</>
-        </div>
-        }
+        {activeCategory === 0 ? <Product /> : <Settings />}
       </div>
     </div>
   </Container>
