9cdcd468722f872b5a6559cc7211bb34a54519c2
Ross Ragsdale <ross.ragsdale@gmail.com>
2020-12-27 18:01:46 -0600

    Create bigcommerce API on lambda.

--- amplify/backend/function/AppSyncDispatcher/src/index.js
+++ amplify/backend/function/AppSyncDispatcher/src/index.js
@@ -5,16 +5,40 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
+var request = require('request')
+
+var options = {
+  method: 'POST',
+  url: 'https://api.bigcommerce.com/stores/mvk5ue8r3o/v3/catalog/products',
+  headers: {
+    'content-type': 'application/json',
+    accept: 'application/json',
+    'x-auth-token': process.env.ACCESS_TOKEN,
+  },
+  body: {
+    name: 'Create product with image 2',
+    price: '10.00',
+    categories: [23],
+    weight: 4,
+    type: 'physical',
+    images: [
+      {
+        image_url:
+          'https://upload.wikimedia.org/wikipedia/commons/7/7f/Anglel_Bless_Legendary_Hills_1_m%C4%9Bs%C3%ADc_st%C3%A1%C5%99%C3%AD.jpg',
+      },
+    ],
+  },
+  json: true,
+}
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
+  const data = await new Promise(function (resolve, reject) {
+    request(options, function (error, response, body) {
+      if (error) reject(error)
+      resolve(body)
+    })
+  })
+
+  console.log(data)
+  return { statusCode: 200 }
+}
--- pages/vendor/dashboard.tsx
+++ pages/vendor/dashboard.tsx
@@ -1,30 +1,50 @@
 import cn from 'classnames'
-import { useState } from "react"
+import { useState } from 'react'
 import { Layout } from '@components/common'
 import { Container, Products, Settings } from '@components/ui'
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react'
+
+import { Amplify } from 'aws-amplify'
+import awsExports from '../../lib/aws-exports'
+
+Amplify.configure({ ...awsExports, ssr: true })
+
 export default function Dashboard() {
-  const categories = [{index: 0, label: 'Product'}, {index: 1, label: 'Settings'}]
+  const categories = [
+    { index: 0, label: 'Product' },
+    { index: 1, label: 'Settings' },
+  ]
   const [activeCategory, setActiveCategory] = useState(0)
 
-  return <Container>
-    <div className="grid grid-cols-12 gap-4 mt-3 mb-20">
-      <div className="col-span-2">
-          <ul>
-            <li className="py-1 text-base font-bold tracking-wide">
-              <a>Vendor Dashboard</a>
-            </li>
-            {categories.map(({ index, label }) => (
-              <li onClick={() => setActiveCategory(index)} className={cn("flex items-center mt-4 py-2 px-6 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100", { underline: activeCategory === index })}>
-                <a className="btn-blue">{label}</a>
+  return (
+    <Container>
+      <AmplifyAuthenticator>
+        <div className="grid grid-cols-12 gap-4 mt-3 mb-20">
+          <div className="col-span-2">
+            <ul>
+              <li className="py-1 text-base font-bold tracking-wide">
+                <a>Vendor Dashboard</a>
               </li>
-            ))}
-          </ul>
-      </div>
-      <div className="col-span-10">
-        {activeCategory === 0 ? <Products /> : <Settings />}
-      </div>
-    </div>
-  </Container>
+              {categories.map(({ index, label }) => (
+                <li
+                  onClick={() => setActiveCategory(index)}
+                  className={cn(
+                    'flex items-center mt-4 py-2 px-6 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100',
+                    { underline: activeCategory === index }
+                  )}
+                >
+                  <a className="btn-blue">{label}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-span-10">
+            {activeCategory === 0 ? <Products /> : <Settings />}
+          </div>
+        </div>
+      </AmplifyAuthenticator>
+    </Container>
+  )
 }
 
 Dashboard.Layout = Layout
