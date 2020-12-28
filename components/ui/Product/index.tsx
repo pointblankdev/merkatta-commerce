5b8251700b212e8f4e2c2474aeb60ba2570de4a9
danstepanov <danhstepanov@gmail.com>
2020-12-28 02:28:02 -0800

    add unit dropdown to add product form

--- components/ui/Product/index.tsx
+++ components/ui/Product/index.tsx
@@ -25,6 +25,9 @@ const ProductItem = ({ props }) => (
       </div>
     </td>
     <td className="px-6 py-4 whitespace-nowrap">
+      <div className="text-sm text-gray-900">{props.unit}</div>
+    </td>
+    <td className="px-6 py-4 whitespace-nowrap">
       <div className="text-sm text-gray-900">{props.sku}</div>
     </td>
     <td className="px-6 py-4 whitespace-nowrap">
@@ -48,6 +51,9 @@ const ProductHeader = () => (
         Item
       </th>
       <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+        Unit
+      </th>
+      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
         SKU
       </th>
       <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -69,6 +75,7 @@ const Products = () => {
   const [products, setProducts] = useState([
     {
       title: "T-shirt",
+      unit: 'MSI',
       description: "It's a shirt that belongs to dan.",
       image: "",
       sku: '123',
@@ -78,17 +85,25 @@ const Products = () => {
   ])
   const [hideProductModal, setHideProductModal] = useState(true)
   const [title, setTitle] = useState('')
+  const [unit, setUnit] = useState('')
   const [description, setDescription] = useState('')
+  const [showUnitDropdown, setShowUnitDropdown] = useState(false)
   const [SKU, setSKU] = useState('')
   const [price, setPrice] = useState('')
 
   const addProduct = () => {
-    setProducts([...products, { title, description, image: "", sku: SKU, status: 'Active', price }])
+    setProducts([...products, { title, unit, description, image: "", sku: SKU, status: 'Active', price }])
     setTitle('')
+    setUnit('')
     setDescription('')
     setSKU('')
     setPrice('')
     setHideProductModal(true)
+  }
+
+  const selectUnitDropdown = unit => {
+    setUnit(unit)
+    setShowUnitDropdown(false)
   }
 
     return <Container>
@@ -115,6 +130,31 @@ const Products = () => {
                       <form onSubmit={addProduct}>
                         <div className="flex flex-col space-y-4 w-full my-6">
                           <Input placeholder="name" onChange={setTitle} />
+                          <div className="relative inline-block text-left">
+                            <div>
+                              <button type="button" onClick={() => setShowUnitDropdown(!showUnitDropdown)} className="inline-flex justify-between w-full border border-gray-200 shadow-sm px-6 py-3 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" aria-haspopup="true" aria-expanded="true">
+                                {unit || 'units'}
+                                <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                  <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                              </button>
+                            </div>
+                            {showUnitDropdown && (
+                              <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                                <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                  <button type="button" onClick={() => selectUnitDropdown('MSI')} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">
+                                    MSI
+                                  </button>
+                                  <button type="button" onClick={() => selectUnitDropdown('lbs')} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">
+                                    Pounds
+                                  </button>
+                                  <button type="button" onClick={() => selectUnitDropdown('Linear ft')} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">
+                                    Linear Feet
+                                  </button>
+                                </div>
+                              </div>
+                            )}
+                          </div>
                           <Input placeholder="description" onChange={setDescription} />
                           <Input placeholder="SKU" onChange={setSKU} />
                           <Input placeholder="price" onChange={setPrice} />
@@ -125,7 +165,7 @@ const Products = () => {
                 </div>
               </div>
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button disabled={title === '' || description === '' || SKU === '' || price === ''} onClick={addProduct} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                <button disabled={title === '' || description === '' || unit === '' || SKU === '' || price === ''} onClick={addProduct} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                   Add
                 </button>
                 <button onClick={() => setHideProductModal(true)} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
