42e1901e7b7d7cdaf4688a42291899b9e856cf19
danstepanov <danhstepanov@gmail.com>
2020-12-28 02:44:36 -0800

    modal background covers all elements on screen when modal is presented

--- components/ui/Product/index.tsx
+++ components/ui/Product/index.tsx
@@ -110,7 +110,7 @@ const Products = () => {
       <div className="mb-12 transition ease-in duration-75" >
         <h1 className="flex-auto text-xl font-semibold">Products</h1>
         {!hideProductModal && (
-          <div className={"fixed z-10 inset-0 overflow-y-auto"}>
+          <div className={"fixed z-40 inset-0 overflow-y-auto"}>
           <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div className="fixed inset-0 transition-opacity" aria-hidden="true">
               <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
