db8d98fbc81d592787f4c27a91c5e1f5fa9d1298
James Schott <james.w.schott@gmail.com>
2021-02-08 14:27:40 -0800

    add signout to avatar in navbar

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -12,6 +12,8 @@ const Navbar: FC = () => {
   const [hasScrolled, setHasScrolled] = useState(false)
   const router = useRouter()
 
+  const [avatarOpen, setAvatarOpen] = useState(false)
+
   const handleScroll = () => {
     const offset = 0
     const { scrollTop } = document.documentElement
@@ -67,11 +69,25 @@ const Navbar: FC = () => {
           {router.pathname.includes('/vendor/dashboard') && (
             <>
               <div className="flex flex-1 justify-end space-x-8">
-                <Link href="/api/vendor/logout">
-                  <a>
-                    <Avatar />
-                  </a>
-                </Link>
+                <Avatar onClick={() => setAvatarOpen(!avatarOpen)}/>
+                {
+                avatarOpen && (  
+                  <>
+                    <div className="origin-top-right absolute top-12 right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                      <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Account settings</a>
+                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Support</a>
+                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">License</a>
+                        <Link href="/api/vendor/logout">
+                          <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">
+                            Sign Out
+                          </a>
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="fixed h-full w-full" onClick={() => setAvatarOpen(!avatarOpen)}/>
+                    </>
+                    )}
               </div>
             </>
           )}
