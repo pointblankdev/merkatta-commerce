01fb2d8e64c5595146e3b00a1de144396de8f055
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-02-08 20:53:10 -0600

    Change name.

--- components/ui/Product/index.tsx
+++ components/ui/Product/index.tsx
@@ -1,8 +1,42 @@
 import { useState } from 'react'
 import { Container } from '@components/ui'
-import ProductForm, { form } from './Form'
+import ProductForm, { store } from './Form'
 import { cloneDeep } from 'lodash'
 import ProductTable from './Table'
+
+const ProductView = (props) => {
+  const [products] = useState(props.products || [])
+  const [hideProductModal, setHideProductModal] = useState(true)
+
+  return (
+    <Container>
+      <div className="mb-12 transition ease-in duration-75">
+        <h1 className="flex-auto text-xl font-semibold">Products</h1>
+        <AddProductModal
+          hide={hideProductModal}
+          closeModal={() => setHideProductModal(true)}
+        />
+        {products.length > 0
+          ? (
+          <div className="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <ProductTable products={products} />
+          </div>
+            )
+          : (
+          <ProductEmpty />
+            )}
+        <button
+          onClick={() => setHideProductModal(false)}
+          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+        >
+          Add Product
+        </button>
+      </div>
+    </Container>
+  )
+}
+
+export default ProductView
 
 const ProductEmpty = () => (
   <Container>
@@ -41,7 +75,7 @@ const AddProductModal = ({ hide, closeModal }) => {
           <form
             onSubmit={(e: any) => {
               e.preventDefault()
-              addProduct(cloneDeep(form))
+              addProduct(cloneDeep(store))
             }}
           >
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
@@ -83,37 +117,3 @@ const AddProductModal = ({ hide, closeModal }) => {
     </div>
   )
 }
-
-const Products = (props) => {
-  const [products] = useState(props.products || [])
-  const [hideProductModal, setHideProductModal] = useState(true)
-
-  return (
-    <Container>
-      <div className="mb-12 transition ease-in duration-75">
-        <h1 className="flex-auto text-xl font-semibold">Products</h1>
-        <AddProductModal
-          hide={hideProductModal}
-          closeModal={() => setHideProductModal(true)}
-        />
-        {products.length > 0
-          ? (
-          <div className="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <ProductTable products={products} />
-          </div>
-            )
-          : (
-          <ProductEmpty />
-            )}
-        <button
-          onClick={() => setHideProductModal(false)}
-          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-        >
-          Add Product
-        </button>
-      </div>
-    </Container>
-  )
-}
-
-export default Products
