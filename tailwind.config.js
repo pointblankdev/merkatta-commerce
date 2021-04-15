5a339f3b9c797aec81207153ab2a8a247a2090d1
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-04-15 12:23:45 -0500

    Get eslint and prettier playing nice.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    mode: 'jit',
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: {
-                'berry-red': '#d22356',
-                'barb-orange': '#f15f45',
-                'golden-rod': '#faa73d',
-                teal: '#24a5a9',
-                indigo: '#202253'
-            },
-            fontFamily: {
-                poppins: "'Poppins', sans-serif"
-            }
-        }
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "berry-red": "#d22356",
+        "barb-orange": "#f15f45",
+        "golden-rod": "#faa73d",
+        teal: "#24a5a9",
+        indigo: "#202253",
+      },
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
     },
-    variants: {
-        extend: {}
-    },
-    plugins: []
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
